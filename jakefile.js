5bb19e255f6f3d721d001cb28551e070a672dacc
judas iscariot <mosesabuashia@gmail.com>
2022-07-26 06:04:07 +0100

    .

--- jakefile.js
+++ jakefile.js
@@ -1,5 +1,7 @@
-console.log("WELCOME TO ROCK-PAPER-SCISSORS GAME");
-console.log("Please pick a choice");
+const computer = document.getElementById("computer");
+const user = document.getElementById("usser");
+const gameOver = document.getElementById("gameover");
+const tie = document.getElementById("tie");
 
 let rockbutton = document.getElementById("rock");
 let paperbutton = document.getElementById("paper");
@@ -21,33 +23,79 @@ let computerScore = 1;
 
 function playGame(e) {
   let clickedValue = e.target.id;
+  let computerChoice = computerinput();
   if (playerScore === 5) {
-    console.log("GAMEOVER...You won");
+    gameOver.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "GAMEOVER...You won";
+    gameOver.appendChild(message);
   } else if (computerScore === 5) {
-    console.log("GAMEOVER...I won");
-  } else if (clickedValue === "rock" && computerinput() === "scissors") {
-    console.log("Rock beat scissors: You scored");
-    console.log(playerScore++);
-  } else if (clickedValue === "scissors" && computerinput() === "rock") {
-    console.log("rock beat scissors: I scored");
-    console.log(computerScore++);
-  } else if (clickedValue === "paper" && computerinput() === "rock") {
-    console.log("paper beat rock: You scored");
-    console.log(playerScore++);
-  } else if (clickedValue === "rock" && computerinput() === "paper") {
-    console.log("paper beat rock: I scored");
-    console.log(computerScore++);
-  } else if (clickedValue === "scissors" && computerinput() === "paper") {
-    console.log("scissors beat paper: You scored");
-    console.log(playerScore++);
-  } else if (clickedValue === "paper" && computerinput() === "scissors") {
-    console.log("scisssors beat paper: I scored");
-    console.log(computerScore++);
-  } else if (clickedValue === "scissors" && computerinput() === "scissors") {
-    console.log("scissors meet scissors: Its a tie");
-  } else if (clickedValue === "rock" && computerinput() === "rock") {
-    console.log("rock meet rock: Its a tie");
-  } else if (clickedValue === "paper" && computerinput() === "paper") {
-    console.log("paper meet paper: Its a tie ");
+    gameOver.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "GAMEOVER...I won";
+    gameOver.appendChild(message);
+  } else if (clickedValue === "rock" && computerChoice === "scissors") {
+    user.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "Rock beat scissors: You scored";
+    user.appendChild(message);
+    let score = document.createElement("p");
+    score.textContent = playerScore++;
+    user.appendChild(score);
+  } else if (clickedValue === "scissors" && computerChoice === "rock") {
+    computer.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "Rock beat scissors: I scored";
+    computer.appendChild(message);
+    let score = document.createElement("p");
+    score.textContent = computerScore++;
+    computer.appendChild(score);
+  } else if (clickedValue === "paper" && computerChoice === "rock") {
+    user.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "paper beat rock: You scored";
+    user.appendChild(message);
+    let score = document.createElement("p");
+    score.textContent = playerScore++;
+    user.appendChild(score);
+  } else if (clickedValue === "rock" && computerChoice === "paper") {
+    computer.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "paper beat rock: I scored";
+    computer.appendChild(message);
+    let score = document.createElement("p");
+    score.textContent = computerScore++;
+    computer.appendChild(score);
+  } else if (clickedValue === "scissors" && computerChoice === "paper") {
+    user.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "scissors beat paper: You scored";
+    user.appendChild(message);
+    let score = document.createElement("p");
+    score.textContent = playerScore++;
+    user.appendChild(score);
+  } else if (clickedValue === "paper" && computerChoice === "scissors") {
+    computer.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "scissors beat paper: I scored";
+    computer.appendChild(message);
+    let score = document.createElement("p");
+    score.textContent = computerScore++;
+    computer.appendChild(score);
+  } else if (clickedValue === "scissors" && computerChoice === "scissors") {
+    tie.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "scissors meet scissors: Its a tie";
+    tie.appendChild(message);
+  } else if (clickedValue === "rock" && computerChoice === "rock") {
+    tie.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "rock meet rocks: Its a tie";
+    tie.appendChild(message);
+  } else if (clickedValue === "paper" && computerChoice === "paper") {
+    tie.replaceChildren("");
+    let message = document.createElement("p");
+    message.textContent = "paper meet paper: Its a tie";
+    tie.appendChild(message);
   }
 }
